d8468519101792890e4c0002aa8353c277a19d7b
s.yuvan karthick <98966238+flowlikeariver7S@users.noreply.github.com>
2022-02-19 12:49:10 +0530

    Add files via upload

---
+++ core java/string buff and build/assignmentstring1.java
@@ -0,0 +1,10 @@
+public class assignmentstring1 {
+	public static void main(String[] args) {
+		
+		String str = "Hello World";
+		int count = str.length();
+		System.out.println("The String has " +count +" characters");
+		
+	}
+
+}
